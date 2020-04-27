93658681aaae4fad8fea4915182260a239a5a328
Konrad Knecht <konrad.knecht@gmail.com>
2020-04-27 17:15:22 +0100

    .

--- fyp-godot/note/long_note.gd
+++ fyp-godot/note/long_note.gd
@@ -3,8 +3,10 @@ extends "res://note/base_note.gd"
 var note_len
 var hold_started = false
 var hold_end = false
-var time_delay = 0.2
-var time = 0;
+var time_delay = 0.1
+var time = 0
+var hold = 0
+var note_hitting = false
 
 func _on_ready():
 	._on_ready()
@@ -12,39 +14,46 @@ func _on_ready():
 	$note_beam.scale = Vector3(1,1,note_len)
 			
 func _on_process(delta):
-	._on_process(delta)
-	
-	if gate == null or (gate.note_hit != null and gate.note_hit != self): 
-		return
-	
 	if is_colliding and not hold_end:
 		if gate.is_hitting and not is_hit:
 			hit()
 			hold_started = true
 			gate.note_hit = self
-		elif !gate.is_hitting and hold_started and is_hit:
+		elif !gate.is_hitting and hold_started and is_hit or hold_end:
 			hold_end = true
 			gate.note_hit = null
 			gate.is_hitting = false
 			$note_beam.get_node("MeshInstance").hide()
-			gate.get_node("Animations/long_hit_hold").visible = false
-			gate.get_node("Animations/long_hit_hold").stop()
+			hold_anims(false)
 
 	if hold_started and not hold_end:
 		note_len -= speed.z * delta
-		if note_len > 0:
+		note_hitting = true
+		if note_hitting == true and note_len > 0:
+			print (note_len)
+			hold_anims(true)
 			time += delta
-			gate.get_node("Animations/long_hit_hold").visible = true
-			gate.get_node("Animations/long_hit_hold").play()
 			if time > time_delay:
 				contHit()
 				time = 0
 		else:
-			gate.get_node("Animations/long_hit_hold").visible = false
-			gate.get_node("Animations/long_hit_hold").stop()
+			print("anim stop", gate)
+			note_hitting = false
 			
+		if note_hitting == false:
+			hold_anims(false)
+		
+func hold_anims(play):
+	if play == true:
+		gate.get_node("Animations/long_hit_hold").visible = true
+		gate.get_node("Animations/long_hit_hold").play()
+	else:
+		gate.get_node("Animations/long_hit_hold").visible = false
+		gate.get_node("Animations/long_hit_hold").stop()
+		
 func contHit():
 	ui.hit_continued_feedback(accuracy)
+	
 		
 func hit():
 	$MeshInstance.hide()
